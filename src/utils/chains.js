22804e7ca17ce9b7af9ad0f8471632f66143bac6
Ben Whittle <benwhittle31@gmail.com>
2023-03-13 19:42:22 -0500

    add goerli

--- src/utils/chains.js
+++ src/utils/chains.js
@@ -1,11 +1,13 @@
 export const chainIds = {
     ETHEREUM: '1',
+    GOERLI: '5',
     POLYGON: '137',
     MUMBAI: '80001',
 }
 
 export const chainNames = {
     [chainIds.ETHEREUM]: 'Ethereum',
+    [chainIds.GOERLI]: 'Goerli',
     [chainIds.POLYGON]: 'Polygon',
     [chainIds.MUMBAI]: 'Mumbai',
 }
