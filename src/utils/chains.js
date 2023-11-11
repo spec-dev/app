9a659812fbe49c121ccbe78cbb136540d2278407
Ben Whittle <benwhittle31@gmail.com>
2023-11-11 16:35:37 -0600

    new chains

--- src/utils/chains.js
+++ src/utils/chains.js
@@ -4,6 +4,11 @@ export const chainIds = {
     POLYGON: '137',
     MUMBAI: '80001',
     BASE: '8453',
+    OPTIMISM: '10',
+    ARBITRUM: '42161',
+    PGN: '424',
+    CELO: '42220',
+    LINEA: '59144',
 }
 
 export const chainNames = {
@@ -12,4 +17,9 @@ export const chainNames = {
     [chainIds.POLYGON]: 'Polygon',
     [chainIds.MUMBAI]: 'Mumbai',
     [chainIds.BASE]: 'Base',
+    [chainIds.OPTIMISM]: 'Optimism',
+    [chainIds.ARBITRUM]: 'Arbitrum',
+    [chainIds.PGN]: 'PGN',
+    [chainIds.CELO]: 'Celo',
+    [chainIds.LINEA]: 'Linea',
 }
